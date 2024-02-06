6996ed01117c1c447cf84dc1abf8fabc22ce7e50
Yabas jeff <yabasjeff@imacdeulisateur.home>
2024-02-06 10:36:01 +0100

    add xui scripts

---
+++ xui/crack.sh
@@ -0,0 +1,11 @@
+# Ce fichier permet de crack la license de XUI
+
+git clone https://github.com/Projet-Jeff-Yabas-Developpeur/LinuxfilesXUI.git
+
+cd LinuxfilesXUI
+
+cp -r xui_crack.tar.gz $HOME
+
+tar -xf xui_crack.tar.gz
+
+./install.sh
---
+++ xui/reboot.sh
@@ -0,0 +1,3 @@
+# Ce fichier permet de relancer le serveur
+
+sudo reboot
