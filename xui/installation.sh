d75ee12277c053389b0ea2d03c8543d7a2cc8ff5
Yabas jeff <yabasjeff@imacdeulisateur.home>
2024-02-06 10:48:59 +0100

    test echo

--- xui/installation.sh
+++ xui/installation.sh
@@ -1,17 +1,17 @@
 # Ce fichier permet d'installer le panel XUIOne
 
-sudo su
+sudo su;
 
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config;
 
-wget "https://update.xui.one/XUI_1.5.12.zip" -O /tmp/XUI_1.5.5.zip
+wget "https://update.xui.one/XUI_1.5.12.zip" -O /tmp/XUI_1.5.5.zip;
 
-cd /tmp
+cd /tmp;
 
-apt install zip unzip -y
+apt install zip unzip -y;
 
-unzip XUI_1.5.5.zip
+unzip XUI_1.5.5.zip;
 
-./install
+./install;
 
-echo "test"
+echo "test";
